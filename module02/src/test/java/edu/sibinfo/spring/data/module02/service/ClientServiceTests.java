85578642697909cca90a9a24346b7536a00f8f36
Kirill Karmakulov <kkarmakul@gmail.com>
2017-05-16 23:37:39 +0700

    Module 02 - @Transactional (test isolation)

--- module02/src/test/java/edu/sibinfo/spring/data/module02/service/ClientServiceTests.java
+++ module02/src/test/java/edu/sibinfo/spring/data/module02/service/ClientServiceTests.java
@@ -1,8 +1,10 @@
 package edu.sibinfo.spring.data.module02.service;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.sibinfo.spring.data.module02.AppRunner;
 import edu.sibinfo.spring.data.module02.dao.ClientDao;
@@ -20,6 +23,7 @@ import edu.sibinfo.spring.data.module02.service.impl.SmsService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class ClientServiceTests {
     @Autowired
     private ClientService service;
@@ -32,15 +36,25 @@ public class ClientServiceTests {
     
     @Test
     public void clientRegisters() {
-        Client client = service.register("A", "B", "7");
+        clientRegistration("A", "B", "7");
+    }
+
+    @Test
+    public void clientRegistersCyrillic() {
+        clientRegistration("А", "Б", "+7");
+    }
+
+	private void clientRegistration(String firstName, String familyName, String mobile) {
+		Client client = service.register(firstName, familyName, mobile);
+        assertThat(dao.count(), equalTo(1L));
         Client realClient = dao.findOne(client.getId());
         assertNotNull(realClient);
-        assertEquals("A", realClient.getFirstName());
-        assertEquals("B", realClient.getFamilyName());
-        assertEquals("7", realClient.getMobile());
+        assertEquals(firstName, realClient.getFirstName());
+        assertEquals(familyName, realClient.getFamilyName());
+        assertEquals(mobile, realClient.getMobile());
         
         ArgumentCaptor<ClientRegisteredEvent> captor = ArgumentCaptor.forClass(ClientRegisteredEvent.class); 
         verify(smsService).sendRegistrationNotification(captor.capture());
-        assertSame(realClient, captor.getValue().getClient());
-    }
+        assertSame(client, captor.getValue().getClient());
+	}
 }
