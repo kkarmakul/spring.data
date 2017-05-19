ca22e464aec20809ed3ae7be93dd593ca5644e8a
Kirill Karmakulov <kkarmakul@gmail.com>
2017-05-20 01:05:16 +0700

    Module 02 - Tiny Test Context #3
    @SpringBootTest
    @ContextConfiguration(classes = ...)
    public class ClientServiceNoDaoTests

---
+++ module02/src/test/java/edu/sibinfo/springtest/data/module02/service/ClientServiceNoDaoTests.java
@@ -0,0 +1,49 @@
+package edu.sibinfo.springtest.data.module02.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import edu.sibinfo.spring.data.module02.domain.Client;
+import edu.sibinfo.spring.data.module02.service.ClientRegisteredEvent;
+import edu.sibinfo.spring.data.module02.service.ClientService;
+import edu.sibinfo.spring.data.module02.service.impl.SmsService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = ClientServiceNoDaoTestsConfig.class)
+public class ClientServiceNoDaoTests {
+	@Autowired
+	private ClientService service;
+	@Autowired
+	private SmsService smsService;
+
+	@Test
+	public void clientRegisters() {
+		clientRegistration("A", "B", "7");
+	}
+
+	@Test
+	public void clientRegistersCyrillic() {
+		clientRegistration("А", "Б", "+7");
+	}
+
+	private void clientRegistration(String firstName, String familyName, String mobile) {
+		Client client = service.register(firstName, familyName, mobile);
+		assertEquals(firstName, client.getFirstName());
+		assertEquals(familyName, client.getFamilyName());
+		assertEquals(mobile, client.getMobile());
+
+		ArgumentCaptor<ClientRegisteredEvent> captor = ArgumentCaptor.forClass(ClientRegisteredEvent.class);
+		verify(smsService).sendRegistrationNotification(captor.capture());
+		assertSame(client, captor.getValue().getClient());
+	}
+}
